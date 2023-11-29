40e62367a52af398e2755798cfdaef17eb8b531c
raftow <rboubaker@tvtc.gov.sa>
2023-11-29 17:54:47 +0300

    denied access nice

--- hzm/web/hzm_basic_header.php
+++ hzm/web/hzm_basic_header.php
@@ -11,22 +11,22 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=<?=$page_charset?>">
-<!-- <link type="text/css" rel="stylesheet" href="../lib/css/simple/pag_checkboxes.css"> -->
+<!-- <link type="text/css" rel="stylesheet" href="../../css/simple/pag_checkboxes.css"> -->
 <!-- plugins -->
-<link type="text/css" rel="stylesheet" href="../lib/bmulti/css/bootstrap-multiselect.css"/>
+<link type="text/css" rel="stylesheet" href="../../bmulti/css/bootstrap-multiselect.css"/>
 <!-- end plugins -->
-<link rel="stylesheet" href="../lib/css/jquery-ui-1.11.4.css">
-<link rel="stylesheet" href="../lib/css/font-awesome.min.css">
-<link rel="stylesheet" href="../lib/css/menu_<?=$lang?>.css">
-<link rel="stylesheet" href="../lib/css/front-application.css">
-<link rel="stylesheet" href="../lib/css/front_app.css">
-<link rel="stylesheet" href="../lib/bootstrap/bootstrap-v3.min.css">
-<link rel="stylesheet" href="../lib/bsel/css/bootstrap-select.css">
+<link rel="stylesheet" href="../../css/jquery-ui-1.11.4.css">
+<link rel="stylesheet" href="../../css/font-awesome.min.css">
+<link rel="stylesheet" href="../../css/menu_<?=$lang?>.css">
+<link rel="stylesheet" href="../../css/front-application.css">
+<link rel="stylesheet" href="../../css/front_app.css">
+<link rel="stylesheet" href="../../bootstrap/bootstrap-v3.min.css">
+<link rel="stylesheet" href="../../bsel/css/bootstrap-select.css">
 <link href="./css/logo-app-icon.png" rel="shortcut icon">
 <title>Momken Framework</title>
 
-<link href="../lib/css/simple/style_common.css" rel="stylesheet" type="text/css">
-<link href="../lib/css/simple/style_<?=$lang?>.css" rel="stylesheet" type="text/css">
+<link href="../../css/simple/style_common.css" rel="stylesheet" type="text/css">
+<link href="../../css/simple/style_<?=$lang?>.css" rel="stylesheet" type="text/css">
 </head>
 
 <body dir="<?=$dir?>" >
