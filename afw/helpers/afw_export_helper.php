69fc4ba5affdde96f3c6932675970bcc6adaa773
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-18 13:35:59 +0300

    Admission & Farz settings and simulator/manager

--- afw/helpers/afw_export_helper.php
+++ afw/helpers/afw_export_helper.php
@@ -17,15 +17,15 @@ class AfwExportHelper
             {
                 if($recursive) $result[$ky] = self::afwExport($varItem);
                 elseif(is_object($var) and ($var instanceof AFWObject)) $result[$ky] = $varItem->getDisplay($lang);
-                else $result = var_export($var, true);
+                else $result[$ky] = $varItem;
             }
         }
         else
         {
-            $result = var_export($var, true);
+            $result = $var;
         }
 
-        return $result;
+        return var_export($result, true);
     }
 
 }
