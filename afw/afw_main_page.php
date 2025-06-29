1dafea5a75882b56f19013856d71d42e9f6d8fbd
rafiq boubaker <rafik.towati@gmail.com>
2025-06-29 12:30:11 +0300

    farz from Tunis

--- afw/afw_main_page.php
+++ afw/afw_main_page.php
@@ -23,7 +23,7 @@ class AfwMainPage
         }
         else
         {
-            die("$special_options_file not found");
+            // die("$special_options_file not found");
         }
 
         return $options;
--- afw/helpers/afw_chart_helper.php
+++ afw/helpers/afw_chart_helper.php
@@ -218,7 +218,7 @@ class AfwChartHelper
         $return =
             "<script>
         new Chart(
-            \"$idCanvas\",
+            document.getElementById('$idCanvas'),
             {
               type: 'bubble',
               options: {
