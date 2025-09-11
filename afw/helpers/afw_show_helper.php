c8e4aaff114e5876059ebecb196ef5037a7bfe7a
rafiq boubaker <rafik.towati@gmail.com>
2025-09-11 14:54:09 +0300

    work on uoh deployment from Tunis

--- afw/helpers/afw_show_helper.php
+++ afw/helpers/afw_show_helper.php
@@ -1936,6 +1936,7 @@ $('#$showAsDataTable').DataTable( {
                     
                     if(($objItem instanceof ApplicationField) and ($col=="qsearch"))
                     {
+                        if($objItem->isEmpty()) throw new AfwRuntimeException("ApplicationField field empty is showing attribute $col");
                         die("objItem instanceof ApplicationField and col==$col, desc=".var_export($desc,true).", structureCol=".var_export($structureCol,true));
                     }
                     if($structureCol['READONLY']) $switcher_authorized = false;
