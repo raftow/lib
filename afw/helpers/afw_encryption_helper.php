c21ddcdccf67768a49758217a28382d8f974a46b
rafiq boubaker <rafik.towati@gmail.com>
2025-12-24 09:19:45 +0300

    work on nauss project

--- afw/helpers/afw_encryption_helper.php
+++ afw/helpers/afw_encryption_helper.php
@@ -10,7 +10,11 @@ class AfwEncryptionHelper
         public static function password_generate($username, $len=7, $numeric=false)
         {
                 if(!$numeric) return substr(md5(rand(4,1000).$username. date("is")),0,$len);
-                else return rand(1111,9999);
+                else {
+                        if($len==5) return rand(11111,99999);
+                        if($len==4) return rand(1111,9999);
+                        return rand(111,999);
+                }
         }
 
 
