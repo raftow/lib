af0a36b0d463fae56ee9c48230002e9aa8ea6799
rafiq boubaker <rafik.towati@gmail.com>
2026-01-04 11:41:00 +0300

    work on nauss project

--- api/afw_col_saver.php
+++ api/afw_col_saver.php
@@ -1,19 +1,19 @@
 <?php
 
 $file_dir_name = dirname(__FILE__);
-require_once("../afw/afw_autoloader.php");
+require_once ('../afw/afw_autoloader.php');
 set_time_limit(8400);
 ini_set('error_reporting', E_ERROR | E_PARSE | E_RECOVERABLE_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
-$lang = "en";
+$lang = 'en';
 
 AfwSession::startSession();
-$update_context = "afw object column value change with network api";
-// echo "here5"; 
-require_once("$file_dir_name/../../config/global_config.php");
+$update_context = 'afw object column value change with network api';
+// echo "here5";
+require_once ("$file_dir_name/../../config/global_config.php");
 // echo "here6";
 // old include of afw.php
 $only_members = true;
-$debug_name = "col saver";
+$debug_name = 'col saver';
 // echo "here4";
 
 $cls = trim($_POST['cls']);
@@ -24,85 +24,61 @@ $val = trim($_POST['val']);
 
 $data = [];
 
-if((!$currmod) or (!$idobj) or (!$cls) or (!$col)) 
-{
-     
-    $data["status"]="error";
-    $data["message"]="afw col saver error : attributes required missed currmod=$currmod idobj=$idobj cls=$cls col=$col";
+if ((!$currmod) or (!$idobj) or (!$cls) or (!$col)) {
+    $data['status'] = 'error';
+    $data['message'] = "afw col saver error : attributes required missed currmod=$currmod idobj=$idobj cls=$cls col=$col";
     die(json_encode($data));
 }
 
-
 $MODULE = $currmod;
-include("$file_dir_name/../lib/afw/afw_check_member.php");
+include ("$file_dir_name/../lib/afw/afw_check_member.php");
 $lang = AfwLanguageHelper::getGlobalLanguage();
- 
+
 // echo "here3";
 AfwAutoLoader::addMainModule($currmod);
-$required_modules = AfwSession::config("required_modules", []);
-foreach($required_modules as $required_module)
-{
+$required_modules = AfwSession::config('required_modules', []);
+foreach ($required_modules as $required_module) {
     AfwAutoLoader::addModule($required_module);
 }
 
-/**
- * @var AFWObject $myObj
- */
+/** @var AFWObject $myObj */
 $myObj = new $cls();
 $myObj_loaded = $myObj->load($idobj);
 
-
-
-
-if(!$myObj_loaded)
-{
-     
-    $data["status"]="error";
-    $data["message"]=$myObj->tm("OBJECT_NOT_FOUND",$lang)." : currmod=$currmod idobj=$idobj cls=$cls";
+if (!$myObj_loaded) {
+    $data['status'] = 'error';
+    $data['message'] = $myObj->tm('OBJECT_NOT_FOUND', $lang) . " : currmod=$currmod idobj=$idobj cls=$cls";
     die(json_encode($data));
 }
 
 $can_popupEditCol = $myObj->userCanPopupEditCol($objme, $col);
-if(!$can_popupEditCol)
-{	
-     
-    $data["status"]="error";
-    $data["message"] = "المعذرة هذه عملية تعديل بوباب سريع جدا على هذا الحقل تحتاج صلاحية ! ";
+if (!$can_popupEditCol) {
+    $data['status'] = 'error';
+    $data['message'] = 'المعذرة هذه عملية تعديل بوباب سريع جدا على هذا الحقل تحتاج صلاحية ! ';
     die(json_encode($data));
 }
 
 list($can_edit_me, $can_t_edit_me_reason) = $myObj->userCanEditMe($objme);
-if(!$can_edit_me)
-{
-     
-    $data["status"]="error";
-    $data["message"] = "المعذرة هذه العملية للتعديل على هذا السجل تحتاج صلاحية : ".$can_t_edit_me_reason;   
+if (!$can_edit_me) {
+    $data['status'] = 'error';
+    $data['message'] = 'المعذرة هذه العملية للتعديل على هذا السجل تحتاج صلاحية : ' . $can_t_edit_me_reason;
 }
 
 $old_val = $myObj->getVal($col);
-if($old_val != $val)
-{
+if ($old_val != $val) {
     $myObj->set($col, $val);
     $done = $myObj->commit();
-}
-else
-{
+} else {
     $done = true;
 }
 
-
-if($done)
-{
-     
-    $data["status"]="success";
-    $data["message"] = "";   
-    $data["aff"] = $myObj->decode($col);   
-}
-else
-{
-     
-    $data["status"]="fail";
-    $data["message"] = "check the beforeUpdate/beforeMaj methods in $cls, because the update has been rejected";   
+if ($done) {
+    $data['status'] = 'success';
+    $data['message'] = '';
+    $data['aff'] = "$myObj => decode($col) = " . $myObj->decode($col);
+} else {
+    $data['status'] = 'fail';
+    $data['message'] = "check the beforeUpdate/beforeMaj methods in $cls, because the update has been rejected";
 }
 
 die(json_encode($data));
