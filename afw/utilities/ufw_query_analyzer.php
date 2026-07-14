d1c767db21d190f2838c439f39ee92dbf1f068f5
rafiq boubaker <rafik.towati@gmail.com>
2026-07-14 19:31:44 +0300

    some important improvement

--- afw/utilities/ufw_query_analyzer.php
+++ afw/utilities/ufw_query_analyzer.php
@@ -181,7 +181,7 @@ class UfwQueryAnalyzer
         );
         $_sql_analysis_seuil_calls = AfwSession::config(
             '_sql_analysis_seuil_calls',
-            12 //000
+            400
         );
 
 
