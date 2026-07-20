81330496a1273004973152c4c9054be8a7e5abb8
rafiq boubaker <rafik.towati@gmail.com>
2026-07-20 13:18:12 +0300

    some important improvement

--- afw/utilities/ufw_query_analyzer.php
+++ afw/utilities/ufw_query_analyzer.php
@@ -324,10 +324,10 @@ class UfwQueryAnalyzer
             $we_can_store_picture_example = (!self::$sql_picture_examples_arr[$this_module][$this_table] or (count(self::$sql_picture_examples_arr[$this_module][$this_table]) < 10));
 
             if ($we_should_store_picture_example and $we_can_store_picture_example) {
+                $theSummerizedBackTrace = "";
                 $backtrace = debug_backtrace(1, 20);
-                $theMainBackTrace = AfwHtmlHelper::theMainBackTrace($backtrace);
-                $theMainBackTrace = "";
-                $sql_picture_example = $sql_query . " >> " . $theMainBackTrace;
+                $theSummerizedBackTrace = AfwHtmlHelper::theSummerizedBackTrace($backtrace);
+                $sql_picture_example = $sql_query . " >> " . $theSummerizedBackTrace;
                 // die($sql_picture_example);
                 self::$sql_picture_examples_arr[$this_module][$this_table][] = $sql_picture_example;
             }
